8dfad21d632aeb0e35e5db65ec2d834fc5b57729
hmtmcse <hmtm.cse@gmail.com>
2023-08-07 17:36:27 +0600

    added flutter framework example project

---
+++ ff_example/README.md
@@ -0,0 +1,2 @@
+# Flutter Framework Example Project (ff_example)
+Flutter Framework all features example project
---
+++ ff_example/android/app/src/main/kotlin/com/example/ff_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ff_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
