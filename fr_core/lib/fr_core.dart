96836b85ef1425db229396b6b3e6f0f1da6b3282
hmtmcse <hmtm.cse@gmail.com>
2023-08-08 13:43:06 +0600

    Export Logic, Service and view classes

--- fr_core/lib/fr_core.dart
+++ fr_core/lib/fr_core.dart
@@ -4,3 +4,8 @@ library fr_core;
 export 'package:flutter/material.dart';
 export 'package:get/get.dart';
 
+
+// Export System Classes
+export 'rapid_logic.dart';
+export 'rapid_service.dart';
+export 'rapid_view.dart';
