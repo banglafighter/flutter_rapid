f18ae64ea875d13d63a2c55a089bab3e91409069
hmtmcse <hmtm.cse@gmail.com>
2023-08-09 20:31:49 +0600

    Added FrOutlinedButton button

--- fr_ui/lib/rapid_button.dart
+++ fr_ui/lib/rapid_button.dart
@@ -87,3 +87,48 @@ class FrElevatedButton extends StatelessWidget {
     );
   }
 }
+
+
+class FrOutlinedButton extends StatelessWidget {
+  final String name;
+  final VoidCallback onPressed;
+  final VoidCallback? onLongPress;
+  final ButtonStyle? style;
+  final double? fontSize;
+  final Color? color;
+  final FontWeight? fontWeight;
+  TextStyle? textStyle;
+  Widget? child;
+
+  FrOutlinedButton({
+    super.key,
+    required this.name,
+    required this.onPressed,
+    this.child,
+    this.onLongPress,
+    this.style,
+    this.textStyle,
+    this.fontSize,
+    this.color,
+    this.fontWeight,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    textStyle ??= TextStyle(
+      fontSize: fontSize,
+      color: color,
+      fontWeight: fontWeight,
+    );
+    Widget childWidget = child ??= Text(
+      name,
+      style: textStyle,
+    );
+    return OutlinedButton(
+      onPressed: onPressed,
+      style: style,
+      onLongPress: onLongPress,
+      child: childWidget,
+    );
+  }
+}
