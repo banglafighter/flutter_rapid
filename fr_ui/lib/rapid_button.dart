5b99d27bf38d6e211bf42c6e6e72f46aedb09e04
hmtmcse <hmtm.cse@gmail.com>
2023-08-09 15:00:23 +0600

    Added Rapid Elevated button

--- fr_ui/lib/rapid_button.dart
+++ fr_ui/lib/rapid_button.dart
@@ -26,12 +26,60 @@ class FrTextButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    textStyle ??= TextStyle(fontSize: fontSize, color: color, fontWeight: fontWeight);
+    textStyle ??= TextStyle(
+      fontSize: fontSize,
+      color: color,
+      fontWeight: fontWeight,
+    );
     Widget childWidget = child ??= Text(
       name,
       style: textStyle,
     );
     return TextButton(
+      onPressed: onPressed,
+      style: style,
+      onLongPress: onLongPress,
+      child: childWidget,
+    );
+  }
+}
+
+class FrElevatedButton extends StatelessWidget {
+  final String name;
+  final VoidCallback onPressed;
+  final VoidCallback? onLongPress;
+  final ButtonStyle? style;
+  final double? fontSize;
+  final Color? color;
+  final FontWeight? fontWeight;
+  TextStyle? textStyle;
+  Widget? child;
+
+  FrElevatedButton({
+    super.key,
+    required this.name,
+    required this.onPressed,
+    this.child,
+    this.onLongPress,
+    this.style,
+    this.textStyle,
+    this.fontSize,
+    this.color,
+    this.fontWeight,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    textStyle ??= TextStyle(
+      fontSize: fontSize,
+      color: color,
+      fontWeight: fontWeight,
+    );
+    Widget childWidget = child ??= Text(
+      name,
+      style: textStyle,
+    );
+    return ElevatedButton(
       onPressed: onPressed,
       style: style,
       onLongPress: onLongPress,
