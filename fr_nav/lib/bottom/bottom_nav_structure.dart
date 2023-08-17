fa07cf51f5306bc21ce90ed886d823599417af6d
hmtmcse <hmtm.cse@gmail.com>
2023-08-17 21:01:57 +0600

    Added Navigation Data

--- fr_nav/lib/bottom/bottom_nav_structure.dart
+++ fr_nav/lib/bottom/bottom_nav_structure.dart
@@ -1,3 +1,23 @@
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+import 'package:fr_core/fr_core.dart';
+
 typedef BottomNavTap = void Function(int index, String routeName);
 
 abstract class BottomNavData {}
+
+class ConvexBottomNavData extends BottomNavData {
+  Color? color;
+  Color? activeColor;
+  Color? backgroundColor;
+  Color? shadowColor;
+  TapNotifier? onTabNotify;
+  TabStyle? style;
+
+  ConvexBottomNavData({
+    this.color,
+    this.activeColor,
+    this.backgroundColor,
+    this.shadowColor,
+    this.style,
+  });
+}
